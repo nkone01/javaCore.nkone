3cfbb7ef690ab8a3c50f9184299abf99c62e73b4
nkone01 <alexnkone82@gmail.com>
2026-02-03 14:34:15 +0100

    Add files via upload

---
+++ PracticeVariablePrimitive.java
@@ -0,0 +1,20 @@
+package javacore.chapter01.variable.practice;
+
+public class PracticeVariablePrimitive {
+
+    public static void main(String[] args) {
+
+        byte age1   = 127;
+        short age2  = 32767;
+        int age3    = 2146725555;
+        long age4   = 999999999999999999L;
+
+        float temperature1 =  1.0F;
+        double temperature2 = 2.0;
+
+        char tabulation     = ' ';
+
+        boolean vrai  = true;
+        boolean faux  = false;
+    }
+}
