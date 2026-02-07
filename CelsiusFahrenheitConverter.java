649eb64590fdf9ec705e67585cc3044f21fbfd13
nkone01 <alexnkone82@gmail.com>
2026-02-07 18:09:28 +0100

    Add files via upload

---
+++ CelsiusFahrenheitConverter.java
@@ -0,0 +1,23 @@
+package javacore.chapter01.variable.exercice;
+
+public class CelsiusFahrenheitConverter {
+
+    public static void main(String[] args) {
+
+        double temperatureEnCelsius = 30.0;
+        // Appliquer la formule de conversion Celsius vers Fahrenheit
+        double temperatureEnFahrenheit = (temperatureEnCelsius * 1.8) + 32;
+        // Afficher le résultat
+        System.out.println(temperatureEnCelsius + "°C équivaut à " + temperatureEnFahrenheit + "°F");
+
+
+
+
+    }
+}
+
+
+
+
+
+
